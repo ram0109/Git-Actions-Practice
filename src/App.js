17cc54fe3d409511e5ad7dcd54624e4d3559c84f
Ram <ram.prasad.0768@gmail.com>
2024-08-12 17:40:09 +0530

    updated env

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           "Github Actions Practice Project" - By Ram
+           {process.env.REACT_APP_WELCOME_TEXT || "Github Actions Practice Project"} - By Ram
         </p>
-        <a
-          className="App-link"
-          href="https://youtube.com/@DevOpsPro"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn DevOps
-        </a>
+        
       </header>
     </div>
   );
